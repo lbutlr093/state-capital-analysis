347f5c50828dbed8bebd6463a067f0f6448c1fb2
Logan <logan@Logans-MacBook-Pro.local>
2017-07-03 20:22:54 -0500

    added column for ratio, cleaned up a bit

--- state_capital_distance.py
+++ state_capital_distance.py
@@ -13,16 +13,18 @@ writer = csv.writer(distance_file)
 with open('Center_of_States.csv', 'rb') as f:
 	reader = csv.reader(f)
 	header = next(reader)
-	header.append('Distance Between (miles)')		# Header for new column
+	header.extend(('Distance Between (miles)', 'Area to Distance Ratio'))		# Header for new columns
 	writer.writerow(header)
 	for line in reader:
-		# If line is blank, do something
+		# TODO: if line is blank, do something
 		coords1 = line[2].split(', ')				# Break coordinates 
 		coords2 = line[4].split(', ')				# into (lat, long)
 		capital_coords = (float(coords1[0]), float(coords1[1]))
 		center_coords = (float(coords2[0]), float(coords2[1]))
 		distance = vincenty(capital_coords, center_coords).miles
+		ratio = float(line[5]) / distance 			# Calculate before rounding distance
 		distance = "{0:.2f}".format(distance)		# Round to 2 decimal places
-		line.append(distance)
+		ratio = "{0:.2f}".format(ratio)
+		line.extend((distance, ratio))
 		writer.writerow(line)
 distance_file.close()
